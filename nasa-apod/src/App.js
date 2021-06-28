9607089a7e8f66ccf040e30662ef802662d618b1
doha <dohaferiadi@gmail.com>
2021-06-28 10:03:25 +0100

    API

---
+++ nasa-apod/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Home from "./components/Home";
+import NasaPhoto from "./components/NasaPhoto";
+import NavBar from './components/NavBar';
+import './App.css';
+
+function App() {
+  return (
+    <BrowserRouter>
+    <div className="app">
+      <Route component={Home} path="/" exact/>
+      <Route component={NasaPhoto} path="/nasaphoto" />
+    </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
